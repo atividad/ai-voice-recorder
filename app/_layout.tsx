5658cb69f87156cddab4f7505f59d97c9592152c
Simon Grimm <saimon@devdactic.com>
2024-10-21 14:38:32 +0200

    Update readme.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,6 +22,26 @@ export default function RootLayout() {
         debug={true}
         options={{
           host: HOST,
+          enableSessionReplay: true,
+          sessionReplayConfig: {
+              // Whether text inputs are masked. Default is true.
+              // Password inputs are always masked regardless
+              maskAllTextInputs: true,
+              // Whether images are masked. Default is true.
+              maskAllImages: true,
+              // Capture logs automatically. Default is true.
+              // Android only (Native Logcat only)
+              captureLog: true,
+              // Whether network requests are captured in recordings. Default is true
+              // Only metric-like data like speed, size, and response code are captured.
+              // No data is captured from the request or response body.
+              // iOS only
+              captureNetworkTelemetry: true,
+              // Deboucer delay used to reduce the number of snapshots captured and reduce performance impact. Default is 500ms
+              androidDebouncerDelayMs: 500,
+              // Deboucer delay used to reduce the number of snapshots captured and reduce performance impact. Default is 1000ms
+              iOSdebouncerDelayMs: 1000,
+          }
         }}>
         <Toaster />
         <Stack>
--- utils/FirebaseConfig.ts
+++ utils/FirebaseConfig.ts
@@ -4,12 +4,12 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCT4T23B1z4xSAZZ0taXy0VYd8NMafyh_8',
-  authDomain: 'voicenotes-92e87.firebaseapp.com',
-  projectId: 'voicenotes-92e87',
-  storageBucket: 'voicenotes-92e87.appspot.com',
-  messagingSenderId: '540762055459',
-  appId: '1:540762055459:web:29a0910d79b4f6f8c0e296',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 // Initialize Firebase
